649ece48efcfd7bd38962ba2858ee35aec9e7c5f
Andrii Kirchei <kircheiandri@gmail.com>
2023-11-06 19:42:38 +0200

    implemented popup plugin for android

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {PopupText} from "../../../popup-text";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hello-app';
+  async handleClick(event: Event){
+    await PopupText.showPopup({message: 'Hello World'})
+
+  }
 }
